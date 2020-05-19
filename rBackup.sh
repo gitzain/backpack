d83f616ce1f7f5082c123a2dde2804502cc766d5
Zain Khan <emailzainkhan@gmail.com>
2020-05-19 01:53:12 +0100

    Revert "Revert "Update rBackup.sh""
    This reverts commit 0d4e7ecd2de3ce7d10000b5d6a11b6d8a5a40b6f.

--- rBackup.sh
+++ rBackup.sh
@@ -14,22 +14,32 @@ exclude_this="Downloads"
 
 #Todays date in ISO-8601 format:
 DAY0=`date -I`
- 
-#Yesterdays date in ISO-8601 format:
-DAY1=`date -I -d "1 day ago"`
- 
+
+
+
+#Get the latest backup directory from the target
+DIRS=`ls $backup_location | awk '{print $NF}' | sort`
+for DIR in $DIRS
+do
+DAY1=${DIR}
+done
+
+
+
 #The source directory:
 SRC="/home/$current_user/"
  
 #The target directory:
 TRG="$backup_location/$DAY0"
-mkdir -p $TRG
+#mkdir -p $TRG
 
 #The link destination directory:
 LNK="$backup_location/$DAY1"
  
 #The rsync options:
 OPT="-avh --delete --exclude=$exclude_hidden --exclude=$exclude_this --link-dest=$LNK"
+
+
  
 #Execute the backup
 rsync $OPT $SRC $TRG
